60d43bc408e48be9ecc03ff6e7a711698a88a553
bicheongan <alj2664@gmail.com>
2026-09-21 16:35:27 +0900

    [Feat]큐브오브젝트에 static int _tagCount 추가

---
+++ Static_Practice/Assets/Scripts/CubeTag.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTag : MonoBehaviour
+{
+    private static int _tagCount;
+    private int _myTag;
+
+
+    private void Awake()
+    {
+        TakeTag();
+    }
+
+    private void TakeTag()
+    {
+        _tagCount++;
+        _myTag = _tagCount;
+
+        Debug.Log($"CubeTag: 내 번호는 {_myTag}이고 지금까지 센 개수는 {_tagCount}");
+    }
+}
