384ce7d233fd02a2e08b4c49dca79b65aaebee18
bicheongan <alj2664@gmail.com>
2026-09-24 23:48:27 +0900

    [Feat] 싱글톤 구성

---
+++ Static_Practice/Assets/Scripts/PracticeScore.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeScore : MonoBehaviour
+{
+    private int _point;
+    public static PracticeScore Instance { get; private set; }
+
+    public int Point => _point;
+
+    private void Awake()
+    {
+        SetSingleton();
+    }
+
+    private void SetSingleton()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    public void AddPoint(int Score)
+    {
+        _point += Score;
+
+        Debug.Log($"PracticeScore: 점수가 {_point}가 되었습니다.");
+    }
+}
